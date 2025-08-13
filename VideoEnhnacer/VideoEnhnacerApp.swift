9a40628c754f67dbc2b4f5eb3407ff9330354430
Vaibhav Tiwary <vaibhav162002@gmail.com>
2025-08-13 10:13:13 +0530

    Initial Commit

---
+++ VideoEnhnacer/VideoEnhnacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEnhnacerApp.swift
+//  VideoEnhnacer
+//
+//  Created by Vaibhav Tiwary on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoEnhnacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
